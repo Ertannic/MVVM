da7dd8e5c5b2392244ecc110d87281d479edfa33
Erzhan <saralaev03@mail.ru>
2024-04-01 18:22:27 +0600

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Ertannic Saralay on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

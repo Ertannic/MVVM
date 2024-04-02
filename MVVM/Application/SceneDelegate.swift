659423e9043cd7d3452230b7884722d2e2eb5acf
Erzhan <saralaev03@mail.ru>
2024-04-02 22:23:04 +0600

    Add TabBar

---
+++ MVVM/Application/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  MVVM
+//
+//  Created by Ertannic Saralay on 01.04.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = TabBarController()
+        window?.rootViewController = UINavigationController(rootViewController: viewController)
+        window?.makeKeyAndVisible()
+    }
+}
+

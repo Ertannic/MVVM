0a607802f7083c31358bd10ac3c73ee5e649e539
Erzhan <saralaev03@mail.ru>
2024-04-04 21:14:50 +0600

    Add tableView

---
+++ MVVM/Models/SettingsItem.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsItem.swift
+//  MVVM
+//
+//  Created by Ertannic Saralay on 04.04.2024.
+//
+
+import Foundation
+
+struct SettingsItem {
+    let title: String
+    let value: String
+}
--- MVVM/Views/SettingsViewController.swift
+++ MVVM/Views/SettingsViewController.swift
@@ -10,18 +10,63 @@ import SnapKit
 
 class SettingsViewController: UIViewController {
     
-    // MARK - Outlets
+    var viewModel = SettingsViewModel()
+    
+    // MARK: - Outlets
     
     private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .plain)
+        let tableView = UITableView(frame: view.bounds, style: .insetGrouped)
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SettingsCell")
         return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.title = "Настройки"
-        
-        
+        title = "Настройки"
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.settingsItems.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.settingsItems[section].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+
+        let item = viewModel.settingsItems[indexPath.section][indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.value
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+               return "Главные Настройки"
+           } else if section == 1 {
+               return "Второстепенные Настройки"
+           }
+           return nil
     }
 }
